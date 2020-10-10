51987fdcf0740f21cbaf0eed11075571992ae99b
Tony Walker <Walkertony509@gmail.com>
2020-10-10 13:11:28 -0700

    MVP Complete

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,7 +2,8 @@ const knex = require("knex");
 
 const knexfile = require("../knexfile.js");
 
-// change to "production" and update knexfile.js to use postgres.
-const database = "development";
+// change to "production" and update knexfile.js to use postgres
+
+const database = "production"
 
 module.exports = knex(knexfile[database]);
